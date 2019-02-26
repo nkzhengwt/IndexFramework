72708d3d5a945691dde183b02526e9659860990d
Wentao Zheng <x1019575585@163.com>
2019-02-26 12:17:54 +0800

    Add files via upload

---
+++ IF/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  30 11:54:11 2019
+
+@author: Wentao
+"""
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+from core import *
+from algos import *
+from backtest import *
+import ffn
+ffn.extend_pandas()
+
+#generate fake data
+data = pd.DataFrame(np.random.uniform(0,900,size=(900, 4)), columns=list('ABCD'), \
+                    index=[pd.Timestamp(datetime.utcnow().date() + timedelta(days=-i)) \
+                           for i in range(900,0,-1)])
+
+#s = Strategy('s1', [RunMonthly(),SelectAll(),WeighERC(),Rebalance()])
+#s = Strategy('s1', [RunQuarterly(),SelectAll(),WeighInvVol(),Rebalance()])
+#s = Strategy('s1', [RunQuarterly(),SelectRandomly(),WeighMeanVar(),Rebalance()],['A','B'])
+
+
+mom_s = Strategy('mom_s', [RunMonthly(),SelectAll(),SelectMomentum(1),WeighEqually(), \
+                              Rebalance()],
+                    ['A', 'B','C','D'])
+
+
+master = Strategy('master', [RunMonthly(),SelectAll(),WeighEqually(),Rebalance()], \
+                    [mom_s, 'A','C'])
+test = Backtest(s, data)
+res = run(test)
+res.plot()
+res.display()
+res.plot_histogram()
+#res.plot_security_weights()
